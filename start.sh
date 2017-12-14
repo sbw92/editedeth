24a413f16fdeb8785aedb1936e1c771e767b31c6
sbw92 <sbw92@outlook.com>
2017-12-14 00:28:21 +0000

    Initial commit

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export GPU_MAX_ALLOC_PERCENT=100
+./ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0x67a748600f4be3502bd3bd2d0bd9fbce2b245422/Multi/shane.buckley.92@gmail.com -mode 1
